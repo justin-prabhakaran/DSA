de05da2e84ca9d495091088a651ebecfd853f22a
justinprabhakaran <c.m.justinprabhakaran@gmail.com>
2024-11-03 21:27:14 +0530

    singly linked list added && some functinality

---
+++ src/test/java/org/justinprabhakaran/SinglyLinkedListTest.java
@@ -0,0 +1,12 @@
+package org.justinprabhakaran;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SinglyLinkedListTest {
+
+    @Test
+    void addElement() {
+    }
+}
